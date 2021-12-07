30e2b917e7fc65bea07221f7ba4ca40f079e30ac
Alexandre Houmeau <alexandre.houmeau@gmail.com>
2021-12-07 16:08:51 +0100

    Fixed add module path

--- src/api/controllers/v1/admin/modules/create.js
+++ src/api/controllers/v1/admin/modules/create.js
@@ -3,30 +3,47 @@ import Request from '@utils/request'
 
 const fields = {
   only: [
-    'title', 'descriptions', 'score'
+    'title', 'description', 'type'
   ]
 }
 
-export default async (req, res, next) => {
+const validation = {
+  rules: {
+    title: 'required|string',
+    type: 'required|string',
+    description: 'required|string'
+  },
+  messages: {
+    'title.required': 'Un titre est requis',
+    'type.required': 'Un type est requis',
+    'description.required': 'Une description est requise'
+  }
+}
+
+const create = async (req, res, next) => {
   try {
+    await Request.validator(req.body, validation.rules, validation.messages)
     const data = await Request.values(req.body, fields)
     const module = await Modules.find({
       title: data.title
     })
 
-    if (module) {
+    if (module?.length) {
       return next(new CatchError('Un module existe déja avec ce nom'))
     }
 
     const newModules = new Modules({
       title: data.title,
-      description: data.description
+      description: data.description,
+      type: data.type
     })
 
     await newModules.save()
-
-    return res.json({ success: true })
+    return res.json({ $message: 'Module bien enregistré' })
   } catch (err) {
-    return next(err)
+    console.log(err)
+    return next(new CatchError(err))
   }
 }
+
+export default create
