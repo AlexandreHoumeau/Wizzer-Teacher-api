cb63edd8880b622e82e8133c52ac2b2e49b18197
Alexandre Houmeau <alexandre.houmeau@gmail.com>
2022-02-02 22:47:13 +0100

    updated course schema

--- src/shared/models/exercices/schema.js
+++ src/shared/models/exercices/schema.js
@@ -5,7 +5,6 @@ import locals from '@shared/mongoose/locals'
 const schema = new Schema({
   // Informations
   title: trimmedString,
-  descriptions: trimmedString,
 
   // Course
   googleDocCourse: trimmedString,
@@ -23,7 +22,7 @@ const schema = new Schema({
   _createdAt: dateDefault,
 
   // Liaisons
-  _sessions: { type: Schema.Types.ObjectId, ref: 'Sessions' },
+  // _sessions: { type: Schema.Types.ObjectId, ref: 'Sessions' },
   _module: { type: Schema.Types.ObjectId, ref: 'Modules' }
 })
 
