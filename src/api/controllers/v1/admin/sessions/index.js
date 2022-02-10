089ea79f41436f5c8472e0f304368e6ac9548890
Alexandre Houmeau <alexandre.houmeau@gmail.com>
2022-02-10 23:18:50 +0100

    epxort controller

--- src/api/controllers/v1/admin/sessions/index.js
+++ src/api/controllers/v1/admin/sessions/index.js
@@ -1,5 +1,6 @@
 import create from './create'
 import get from './get'
 import update from './update'
+import remove from './remove'
 
-export default { create, get, update }
+export default { create, get, update, remove }
