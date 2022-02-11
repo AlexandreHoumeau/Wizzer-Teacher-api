21f3c081d8ee562e1cb5122fcb6e4ac8eb2b8eda
Alexandre Houmeau <alexandre.houmeau@gmail.com>
2022-02-11 18:51:29 +0100

    Created new list controller

---
+++ src/api/controllers/v1/modules/list.js
@@ -0,0 +1,26 @@
+import Modules from '@models/modules'
+
+const list = async (req, res, next) => {
+  try {
+    const modules = await Modules.find()
+
+    if (!modules?.length) {
+      return next(new CatchError('Aucun module trouvé'))
+    }
+
+    const mapped = modules.reduce((acc, module) => {
+      if (module.type === 'dev') {
+        acc.dev.push(module)
+      } else {
+        acc.design.push(module)
+      }
+      return acc
+    }, { dev: [], design: []})
+
+    return res.json({ dev: mapped.dev, design: mapped.design })
+  } catch (err) {
+    return next(new CatchError(err))
+  }
+}
+
+export default list
