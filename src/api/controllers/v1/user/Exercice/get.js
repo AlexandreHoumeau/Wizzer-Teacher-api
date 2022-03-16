51bbc03233c643ba880458e6ce01b2651f8f2834
Alexandre Houmeau <alexandre.houmeau@gmail.com>
2022-03-16 22:54:47 +0100

    added isDone to exercice

--- src/api/controllers/v1/user/Exercice/get.js
+++ src/api/controllers/v1/user/Exercice/get.js
@@ -2,15 +2,23 @@ import Exercice from '@models/exercices'
 
 const get = async (req, res, next) => {
   try {
+    let isDone = false
     const exercice = await Exercice.findById(req.params.exerciceId)
     .populate({
-      path: '_module'
+      path: '_module _tests'
     })
+
     if (!exercice) {
       return next(new NotFoundError('Aucun exercice avec cet identifiant'))
     }
 
-    return res.json({ exercice })
+    const foundTest = exercice._tests.find((t) => String(t._user) === req.user._id)
+
+    if (foundTest) {
+      isDone = true
+    }
+
+    return res.json({ exercice, isDone })
   } catch (error) {
     return next(new CatchError(error))
   }
